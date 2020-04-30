243e879ca3027a75a4d2ab153244d07aed49a6b6
Muslim <ikzsl@yandex.ru>
2020-05-01 00:34:14 +0300

    add middleware

--- src/store.js
+++ src/store.js
@@ -1,9 +1,30 @@
-import {createStore} from "redux";
+import { createStore, applyMiddleware, compose } from 'redux';
 
-import reducer from "./reducers";
+import reducer from './reducers';
 
-const store=createStore(reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    );
+const logMiddleware = ({ getState }) => (next) => (action) => {
+  console.log(action.type, getState());
+  return next(action);
+};
+
+const stringMiddleware = () => (next) => (action) => {
+  if (typeof action === 'string') {
+    return next({
+      type: action,
+    });
+  }
+
+  return next(action);
+};
+
+const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__;
+
+const store = createStore(
+  reducer,
+  compose(
+    applyMiddleware(stringMiddleware, logMiddleware),
+    reduxDevTools && reduxDevTools()
+  )
+);
 
 export default store;
